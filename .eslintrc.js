f16afc896294fd692e886d534f5f445e0bf494be
MK MUZAKKIR <muzakkir7977@gmail.com>
2025-02-03 18:29:21 -0500

    sdf

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+    // ... other configuration settings, such as "extends" ...
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+      // ... any other custom rules ...
+    },
+  };
+  
